07ecc6784f70693ee414817b5574709fa1aa1720
NickNguyen1405 <dong76001@gmail.com>
2025-07-07 12:08:04 +0000

    [ADD] new exercises

--- linked_list_ex3.java
+++ linked_list_ex3.java
@@ -10,3 +10,75 @@
 // if head is null -> ?
 // if list is empty -> ?
 // if head.data == key -> ?
+
+import javax.swing.SingleSelectionModel;
+
+public class linkedListEx3{
+    public static void main(String[]args){
+        SinglyLinkedList exLinkedList = new SinglyLinkedList();
+        exLinkedList.append(10);
+        exLinkedList.append(20);
+        exLinkedList.append(30);
+        exLinkedList.append(20);
+        exLinkedList.deleteNode(10);
+        exLinkedList.printLinkedList();
+    }
+}
+class Node{
+        int data;
+        Node nextNode;
+
+        public Node(int data){
+            this.data = data;
+            this.nextNode = null;
+        }
+}
+
+class SinglyLinkedList{
+        Node head;
+
+    public SinglyLinkedList(){
+        this.head = null;  
+    }
+
+    public void append(int data){
+        Node newNode = new Node(data);
+
+        if(this.head == null){
+        this.head  = newNode;
+        return;
+    }
+        Node last = this.head;
+        while(last.nextNode != null){
+        last = last.nextNode;
+            
+        }
+        last.nextNode = newNode;
+    }
+
+    public void printLinkedList(){
+        Node curNode = this.head;
+        while(curNode != null){
+        System.out.println(curNode.data);
+        curNode = curNode.nextNode;
+        }
+    }
+    public void deleteNode(int key){
+    Node curNode = this.head;
+    Node previousNode = null;
+
+    if(curNode != null && curNode.data == key){
+        this.head = curNode.nextNode;
+        return;
+    }
+    while(curNode != null && curNode.data != key){
+        previousNode = curNode;
+        curNode = curNode.nextNode;
+    }
+    if(curNode == null){
+        return;
+    }
+    previousNode.nextNode = curNode.nextNode;
+    }
+}
+
---
+++ stack.java
@@ -0,0 +1,32 @@
+// Stack: LIFO - Last In First Out
+// rỗ: stack<book>
+// ro.isEmpty() - true
+// ro.push(nguVan12);
+// ro.isEmpty() - false
+// ro.push(giaiTich);
+// ro.push(vatLy);
+// ro.push(hoaHoc);
+
+// ro.peek() - "hoaHoc"
+
+// ro.pop() - "hoaHoc"
+// ro.peek() - "vatLy"
+
+// ro.pop() - "vatLy"
+
+// ro.push(dia)
+// ro.peek() - "dia"
+
+// stack:   push - pop - peek - isEmpty
+
+// Homework: Create a class name ArrayStack<String>
+// constructor: public ArrayStack() 
+//          initialize an empty stack
+// public void push(String name)
+//          insert name to the peek of the Stack
+// public String pop()
+//          delete and return the peek element of the Stack
+// public String peek()
+//          return the peek element of the stack with deleting it
+// publi boolean isEmpty()
+//          return true if the Stack is empty, else if the stack is not empty
